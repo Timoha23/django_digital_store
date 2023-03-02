dc2a1f6e213a9da212a02cf6654454eb9cc99501
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-02 17:24:58 +0300

    добавлены приложения в список установленных, добавлена AUTH_USER_MODEL

--- digital_store/digital_store/settings.py
+++ digital_store/digital_store/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
