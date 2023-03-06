37c884ab1440efd242db93f97c45417e53e20f79
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-06 14:03:21 +0300

    фикс

--- digital_store/users/admin.py
+++ digital_store/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role')
+
+
+admin.site.register(User, UserAdmin)
