50a3a45859f9dcbb7d6db345019a81b66da1862b
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-08 11:26:04 +0300

    инклюд reviews.urls

--- digital_store/digital_store/urls.py
+++ digital_store/digital_store/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('moderation/', include('moderation.urls')),
+    path('review/', include('reviews.urls')),
     path('', include('shop.urls')),
 ]
 
