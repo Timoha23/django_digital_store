acadb242cbfd4940a2e9555d20469f1f409f5de4
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-23 11:56:54 +0300

    добавлен verbose_name

--- digital_store/moderation/apps.py
+++ digital_store/moderation/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ModerationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'moderation'
+    verbose_name = 'Модерация'
