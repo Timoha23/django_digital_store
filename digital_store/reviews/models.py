cb88843cbf026275cbd502325085a58cb9a35f41
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-27 08:07:40 +0300

    isort

--- digital_store/reviews/models.py
+++ digital_store/reviews/models.py
@@ -1,7 +1,7 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from shop.models import User, Product
+from shop.models import Product, User
 
 
 class Review(models.Model):
