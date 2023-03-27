23fed714ab99fab05f2eba22ec8886859a2aeee6
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-27 10:24:09 +0300

    isort + pep

--- digital_store/users/urls.py
+++ digital_store/users/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='users/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'),
+         name='logout'),
     path('favorites/', views.get_favorite_list, name='favorites'),
     path('change_favorite/', views.change_favorite, name='change_favorite'),
     path('profile/<slug:username>/', views.user_profile, name='user_profile'),
