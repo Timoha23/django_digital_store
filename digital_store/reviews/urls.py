be80eb25da433b291c9400010eb575c12bf67ba7
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-27 08:07:19 +0300

    flake8

--- digital_store/reviews/urls.py
+++ digital_store/reviews/urls.py
@@ -6,5 +6,6 @@ app_name = 'review'
 
 urlpatterns = [
     path('add/product/<int:product_id>/', views.review_add, name='review_add'),
-    path('shop/<int:shop_id>/', views.reviews_shop_list, name='reviews_shop_list'),
+    path('shop/<int:shop_id>/', views.reviews_shop_list,
+         name='reviews_shop_list'),
 ]
