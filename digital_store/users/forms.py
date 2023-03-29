022010f231c3416712ed759640d5018270c37485
Ershov Timofey <eqeq.tima@yandex.ru>
2023-03-29 15:44:50 +0300

    добавлена форма редактирования профиля

--- digital_store/users/forms.py
+++ digital_store/users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 from .models import User
 
@@ -7,3 +8,9 @@ class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'email')
+
+
+class ChangeProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image')
