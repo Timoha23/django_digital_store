54ac03d9e77bc09f65c42f20415d1845e11a0c40
Ershov Timofey <eqeq.tima@yandex.ru>
2023-04-02 12:43:23 +0300

    добавлена функция проверки на пустоту корзины

--- digital_store/core/context_processors.py
+++ digital_store/core/context_processors.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 
 from digital_store.settings import STAFF_ROLES
 from shop.models import Shop
+from cart.models import Cart
 
 
 def get_user_have_shop(request):
@@ -21,3 +22,17 @@ def get_user_is_staff(request):
 def get_year(request):
     year = timezone.now().year
     return {'year': year}
+
+
+def count_products_cart(request):
+    """
+    Проверка на пустоту корзины. Если не пуста возвращает количество
+    продуктов в корзине
+    """
+
+    context = {'count_products_cart': None}
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user)
+        if cart.exists():
+            context['count_products_cart'] = cart.count()
+    return context
